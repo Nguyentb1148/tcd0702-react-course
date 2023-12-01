dc0e7fc98000fd14150fad78e1fc10f46c7329f2
Nguyen Tran <nguyentran@Nguyens-MBP.lan>
2023-12-01 14:44:35 +0700

    Refactor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import './App.css';
 import Navbar from './layout/Navbar';
 import Users from './components/User'
-function App() {
-  return (
-    <div className="App">
-     <Navbar/>
-     <div className="container">
-      <Users/>
-     </div>
-    </div>
-  );
-}
+import axios from 'axios';
+import { Component } from 'react';
+class App extends Component{
 
+  state ={
+    usersData:[],
+  };
+  componentDidMount() {
+    axios.get("https://api.github.com/users").then((response) => {
+      this.setState({ usersData: response.data });
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navbar />
+        <div className="container">
+          <Users usersData={this.state.usersData} />
+        </div>
+      </div>
+    );
+  }
+}
 export default App;
+
