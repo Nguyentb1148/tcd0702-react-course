ff5daf0d77cf48f3d7666eda50feaa96aa91cd10
Nguyen Tran <nguyentran@Nguyens-MBP.lan>
2023-12-01 15:32:02 +0700

    Add search funciton

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import Navbar from './layout/Navbar';
-import Users from './components/User'
+import Users from './components/User';
+import Search from './components/Search';
 import axios from 'axios';
 import { Component } from 'react';
 class App extends Component{
 
   state ={
-    usersData:[],
+    usersData:[],// object usersData to store user data
+    searchText:"",// add empty string for search input
   };
-  componentDidMount() {
-    // axios make a get request to github api, then update compoment's state with 
-    // the fetched user data when the request is successfull
-    axios.get("https://api.github.com/users").then((response) => {
-      this.setState({ usersData: response.data });
+
+  // make a request to api github  to search  users name  base on provided text
+  handleSearch=(text)=>{
+    console.log(text);
+    axios.get(`https://api.github.com/search/users?q=${text}`)
+    .then((response)=>{
+      console.log(response);
+      this.setState({usersData: response.data.items});
     });
   }
   render() {
@@ -20,7 +25,8 @@ class App extends Component{
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users usersData={this.state.usersData} />
+         <Search handleSearch={this.handleSearch}/>
+         <Users usersData={this.state.usersData}/>
         </div>
       </div>
     );
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,10 +2,29 @@ import React from "react"
 
 export default class Search extends 
 React.Component{
+
+    state={
+        text:"",// store string to search input
+    };
+
+    handleClick=(e)=>{
+        e.preventDefault();//prevent the default  form submission behavior
+        console.log(this.state.text);
+        this.props.handleSearch(this.state.text);// calls the handleSearch method passed down as a prop from the parent compoment('App')
+
+    }
+
+    handleChange=(e)=>{
+        console.log(e.target.value);
+        this.setState({text:e.target.value})// use to update the compoment state with the new input value
+    }
     render(){
         return(
             <div>
-                <h1>Search</h1>
+                <from>
+                    <input type="text" placeholder="Search" onChange={this.handleChange}/>
+                    <button onClick={this.handleClick} className="btn btn-dark btn-block">Search</button>
+                </from>
             </div>
         )
     }
